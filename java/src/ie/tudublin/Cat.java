7d47ecd56159e44dc1bb5f8562a7a4ddb8971d4d
unknown <c21486544@mytudublin.ie>
2023-02-16 20:29:10 +0000

    added killing the cat

---
+++ java/src/ie/tudublin/Cat.java
@@ -0,0 +1,46 @@
+package ie.tudublin;
+
+public class Cat {
+
+    String name;
+    private int numLives;
+	
+	public void setLives()
+	{
+		this.numLives = 9;
+	}
+
+    public int getNumLives()
+    {
+        return numLives;
+    }
+	
+	public Cat()
+	{
+        System.out.println("created instance\n");
+	}
+	
+	public Cat(String name)
+	{
+        System.out.println(name);
+	}
+
+    public void kill()
+    {
+        if(numLives > 0)
+        {
+            System.out.println("Ouch");
+        }
+        else
+        {
+            System.out.println( "Dead");
+        }
+        numLives = numLives - 1;
+
+    }
+
+    
+}
+
+
+
